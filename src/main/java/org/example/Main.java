8f52e04d6d8ad4f923c977c7bab55b04e0d27ce5
mihay <mihaylov.29ge@mail.ru>
2023-03-18 08:36:26 -0500

    Just added first 5 tasks And commit to see different

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
-package org.example;
+package org.example.homework_nr_1;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class HomeWork1Class {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+        long currentWorldPopulation = 7811080815L;
+        System.out.println(currentWorldPopulation);
+        int carsProducedThisYear =55235358;
+        System.out.println(carsProducedThisYear);
+        char firstLetterInAlphabet = 'A';
+        System.out.println(firstLetterInAlphabet);
+        boolean javaIsAmazing = true;
+        System.out.println(javaIsAmazing);
+        double pi = 3.14159;
+        System.out.println(pi);
     }
 }
