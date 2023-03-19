6d19a7b1cf52057b5be36b4c244480f8a37ad0c9
mihay <mihaylov.29ge@mail.ru>
2023-03-19 14:04:27 -0500

    Finished all work

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example.homework_nr_1;
 
-public class HomeWork1Class {
+class HomeWork1Class {
     public static void main (String[] args) {
         System.out.println("Hello World!");
         long currentWorldPopulation = 7811080815L;
@@ -13,5 +13,17 @@ public class HomeWork1Class {
         System.out.println(javaIsAmazing);
         double pi = 3.14159;
         System.out.println(pi);
+
+        short priceMunaDouble = 3412;
+        System.out.println(priceMunaDouble);
+        short taxMunaDouble = 623;
+        System.out.println(taxMunaDouble);
+        int quantity = 2356;
+        System.out.println(quantity);
+        double sum = 0;
+        System.out.println(sum);
+
+        double tax = priceMunaDouble + taxMunaDouble * quantity + sum;
+        System.out.println(tax);
     }
 }
